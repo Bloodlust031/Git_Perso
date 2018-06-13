54755d9e2567789d435e8a0eb1cef08f02f8c63c
Bloodlust031 <39981246+Bloodlust031@users.noreply.github.com>
2018-06-13 23:01:18 +0200

    Evol Rasp

---
+++ Raspberry/Scripts_Bash/jdownload.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+sudo java -Xmx512m -jar /home/pi/tools/Jdownloader2/JDownloader.jar
+
---
+++ Raspberry/python_projects/LCD/LedGreenOn.py
@@ -0,0 +1,10 @@
+import RPi.GPIO as GPIO                                               # Importation des librairies qui gerent les ports
+import time                                                           # Importation de la librairie temps
+
+GPIO.setwarnings(False)                                               # Mettre sur OFF les alertes (qui sont inutiles)
+GPIO.setmode(GPIO.BCM)                                                # BCM : Numero des GPIO (GPIO 18)
+GPIO.setup(25, GPIO.OUT)                                              # Definition du port en sortie
+
+
+GPIO.output(25, True)                                             # Mise a zero du GPIO 18 (GND)
+
