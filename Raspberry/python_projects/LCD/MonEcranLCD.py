0a64fbf9ec63c198ca9a9ec84df523ea458d3424
Bloodlust031 <39981246+Bloodlust031@users.noreply.github.com>
2018-06-19 09:39:14 +0200

    Rasp/LCD/clean indentation

--- Raspberry/python_projects/LCD/MonEcranLCD.py
+++ Raspberry/python_projects/LCD/MonEcranLCD.py
@@ -35,10 +35,10 @@ def get_cpu_temp():     # get CPU temperature and store it into file "/sys/class
     cpu = tmp.read()
     tmp.close()
     return '{:.1f}'.format( float(cpu)/1000 ) + ' C'
- 
+
 def get_time_now():     # get system time
     return datetime.now().strftime('    %H:%M:%S')
-	
+
 def destroy_LCD():
     global lcd
     global mcp
@@ -50,45 +50,38 @@ def buttonEvent(channel):
     global state_Red
     global state_Red_counter
     #print('buttonEvent GPIO %d' %(channel))
-    
+
     if (state_Red == 0):
-	state_LCD = not state_LCD 
-	if state_LCD : 
-	    print('Turn on LCD Green... ')
-	else : 
-	    print('Turn off LCD Green... ') 
+        state_LCD = not state_LCD 
+        if state_LCD : 
+            print('Turn on LCD Green... ')
+        else : 
+            print('Turn off LCD Green... ') 
     elif(state_Red == 1):
-	state_Red = 2
-	state_Red_counter = 0
+        state_Red = 2
+        state_Red_counter = 0
     elif(state_Red == 2):
-	state_Red = 3
-	state_Red_counter = 0
-    
+        state_Red = 3
+        state_Red_counter = 0
+
 def buttonRedEvent(channel):
     global state_Red
     global state_Red_counter 
     global state_LCD
 
     if (state_Red == 0):
-	state_Red = 1
-	state_Red_counter = 0
-	if state_LCD == False:
-		allum_LCD()
-        else:
-                lcd.clear()
-    elif(state_Red == 1):
-	#annulation
-	state_LCD = True
-	state_Red = 0
-	state_Red_counter = 0
+        state_Red = 1
+        state_Red_counter = 0
+        if state_LCD == False:
+            allum_LCD()
         lcd.clear()
-        state_LCD = True
-        old_state_LCD = True
-	LedOnCounter = 3540	#pour que l'ecran ne reste allume qu'une minute
+    elif(state_Red == 1):
+        #annulation
+        RAZ_red_state()
     elif(state_Red == 2):
-	state_Red = 4
-	state_Red_counter = 0
-    
+        state_Red = 4
+        state_Red_counter = 0
+
 def init_btn():
     global state_Red
     global state_Red_counter    
@@ -96,7 +89,7 @@ def init_btn():
     state_Red_counter = 0    
     GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set buttonPin's mode is input, and pull up to high
     GPIO.add_event_detect(buttonPin,GPIO.FALLING,callback = buttonEvent,bouncetime=300)
-    
+
     GPIO.setup(buttonPinRed, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set buttonPin's mode is input, and pull up to high
     GPIO.add_event_detect(buttonPinRed,GPIO.FALLING,callback = buttonRedEvent,bouncetime=300)
 
@@ -108,14 +101,14 @@ def loop_DHT():
     global dht
     global chk
     global HumExt
-    
+
     chk = dht.readDHT11() 
     if (chk is dht.DHTLIB_OK):  
-	global TempExt
-	TempExt = dht.temperature
-	HumExt = dht.humidity
-	#print"Humidity : %.2f, \t Temperature : %.2f \n"%(dht.humidity,dht.temperature)   	
-		
+        global TempExt
+        TempExt = dht.temperature
+        HumExt = dht.humidity
+        #print"Humidity : %.2f, \t Temperature : %.2f \n"%(dht.humidity,dht.temperature)   	
+
 def init_LCD():
     global lcd
     global mcp
@@ -123,17 +116,31 @@ def init_LCD():
     PCF8574A_address = 0x3F  # I2C address of the PCF8574A chip.
     # Create PCF8574 GPIO adapter.
     try:
-	mcp = PCF8574_GPIO(PCF8574_address)
+        mcp = PCF8574_GPIO(PCF8574_address)
     except:
-	try:
-	    mcp = PCF8574_GPIO(PCF8574A_address)
-	except:
-	    print('I2C Address Error !')
-	    exit(1)
+        try:
+            mcp = PCF8574_GPIO(PCF8574A_address)
+        except:
+            print('I2C Address Error !')
+            exit(1)
     # Create LCD, passing in MCP GPIO adapter.
     lcd = Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4,5,6,7], GPIO=mcp)
-    
-    
+
+def RAZ_red_state():
+    global state_LCD
+    global old_state_LCD
+    global LedOnCounter
+    global state_Red
+    global state_Red_counter        
+    global lcd
+    state_Red = 0
+    state_Red_counter = 0
+    lcd.clear()
+    state_LCD = True
+    old_state_LCD = True
+    LedOnCounter = 3540	#pour que l'ecran ne reste allume qu'une minute
+
+
 def allum_LCD():
     global lcd
     global mcp
@@ -160,56 +167,51 @@ def loop():
     global lcd
 
     while(True):       
-	if (state_Red_counter > 5):
-	    state_Red = 0
-	    state_Red_counter = 0
-            lcd.clear()
-            state_LCD = True
-            old_state_LCD = True
-	    LedOnCounter = 3540	#pour que l'ecran ne reste allume qu'une minute
-	if (state_Red > 0):
-	    state_Red_counter = state_Red_counter + 1
-	    if state_Red ==1:
-		lcd.setCursor(0,0)  # set cursor position
-		lcd.message( "Confirm    Abort" + '\n' )# display CPU temperature
-		message = "%s" % (6-state_Red_counter)
-		lcd.message( message )		
-	    elif state_Red ==2:
-		lcd.setCursor(0,0)  # set cursor position
-		lcd.message( "Reboot  shutdown" + '\n' )# display CPU temperature
-		message = "%s" % (6-state_Red_counter)
-		lcd.message( message )
-	    elif state_Red ==3:
-		print('Reboot')
-		os.system('sudo reboot')
-	    elif state_Red ==4:
-		print('Shutdown')
-		os.system('sudo shutdown -h now')
-	else:
-	    if (LedOnCounter > 3600):
-		state_LCD = False
-	    if (state_LCD == True) and (old_state_LCD == False):
-		#Il faut initialiser
-		allum_LCD()
-	    if (state_LCD == True):
-		loop_DHT()
-		loop_LCD()
-		LedOnCounter  = LedOnCounter  + 1
-	    if ((state_LCD == False) and (old_state_LCD == True)):
-		destroy_LCD()
-	    if (state_LCD == False):
-		LedOnCounter  = 0
-	    old_state_LCD = state_LCD
-	time.sleep(1)	
+        if (state_Red_counter > 5):
+            RAZ_red_state()
+        if (state_Red > 0):
+            state_Red_counter = state_Red_counter + 1
+            if state_Red ==1:
+                lcd.setCursor(0,0)  # set cursor position
+                lcd.message( "Confirm    Abort" + '\n' )# display CPU temperature
+                message = "%s" % (6-state_Red_counter)
+                lcd.message( message )		
+            elif state_Red ==2:
+                lcd.setCursor(0,0)  # set cursor position
+                lcd.message( "Reboot  shutdown" + '\n' )# display CPU temperature
+                message = "%s" % (6-state_Red_counter)
+                lcd.message( message )
+            elif state_Red ==3:
+                print('Reboot')
+                os.system('sudo reboot')
+            elif state_Red ==4:
+                print('Shutdown')
+                os.system('sudo shutdown -h now')
+        else:
+            if (LedOnCounter > 3600):
+                state_LCD = False
+            if (state_LCD == True) and (old_state_LCD == False):
+                #Il faut initialiser
+                allum_LCD()
+            if (state_LCD == True):
+                loop_DHT()
+                loop_LCD()
+                LedOnCounter  = LedOnCounter  + 1
+            if ((state_LCD == False) and (old_state_LCD == True)):
+                destroy_LCD()
+            if (state_LCD == False):
+                LedOnCounter  = 0
+            old_state_LCD = state_LCD
+        time.sleep(1)	
 
 if __name__ == '__main__':
     init_LCD()
     init_DHT()
     init_btn()
     try:
-	loop()
+        loop()
     except KeyboardInterrupt:
-	destroy_LCD()
-	GPIO.cleanup()
-	exit()  	
-	GPIO.output(25, False)
+        destroy_LCD()
+        GPIO.cleanup()
+        exit()  	
+        GPIO.output(25, False)
