3ae96298ee7235a322c438f97f94826f7ec64919
Bloodlust031 <39981246+Bloodlust031@users.noreply.github.com>
2020-11-19 21:14:47 +0100

    Update Extract_VCI_files_json.py

--- Python_Eclipse/ProjetPython/TestJson/Extract_VCI_files_json.py
+++ Python_Eclipse/ProjetPython/TestJson/Extract_VCI_files_json.py
@@ -19,8 +19,6 @@ to_integrate_Msg_cnt = False
 chemin_base = 'H:/Boulot/Boulot Jeje/Main/Ican/Extract_traces_FTP/TempDownAWSS3/'
 #chemin_base = 'D:/Boulot/Main/Ican/Extract_traces_FTP/TempDownAWSS3/'
 #chemin_base = 'D:/temp/DWLD msg Bastides/'
-fichier_log = 'D:/temp/log_tata.txt'
-fichier_log = 'D:/temp/JsonOut.json'
 path_sortie = 'D:/temp/'
 prefixe_nom_sortie = 'Msg_IMEI_'
 
@@ -29,20 +27,21 @@ current_dict_messages = dict()
 
 def listdirectory(path): 
     liste_fichier=[] 
+    #for root, dirs, files in os.walk(path): 
     for root, dirs, files in os.walk(path): 
         for i in files: 
             liste_fichier.append(os.path.join(root, i)) 
     return liste_fichier
 
 
-def traite_1_1fic(path): 
+def traite_1_1fic(nom_fic_msg): 
     #print("fichier en cours: ", path)
     #print("fichier en cours: ", os.path.basename(path))
     current_msg = dict()
     global current_IMEI
     global current_dict_messages
     
-    with open(nom_fic) as json_file2:
+    with open(nom_fic_msg) as json_file2:
         data = json.load(json_file2)
         #print(data['tim'], data['int'])
         #print(data['evt'], data['eid'])
@@ -72,6 +71,9 @@ def traite_1_1fic(path):
         current_msg['Delai_GSM'] = (data['int']-data['tim'])/1000
         current_msg['VIN'] = str(data['vin'])
         
+        if current_msg['VIN'] not in current_dict_messages["VIN_list"]:
+            current_dict_messages["VIN_list"].append(current_msg['VIN'])
+        
         if to_integrate_Msg_bin:
             current_msg['Msg_brut'] = str(data['bin'])
         if to_integrate_Msg_cnt:
@@ -84,9 +86,9 @@ def traite_1_1fic(path):
         current_dict_messages["Msg_list"].append(dict(current_msg))
     current_msg.clear()
 
-def lire_dictionnaire_messages(path, strIMEI):
+def lire_dictionnaire_messages(chemin, strIMEI):
     global current_dict_messages
-    nom_fic = path + strIMEI + '.json'
+    nom_fic = chemin + strIMEI + '.json'
     current_dict_messages.clear()
     if os.path.isfile(nom_fic):
         with open(nom_fic, 'r') as json_file_result:
@@ -97,16 +99,16 @@ def lire_dictionnaire_messages(path, strIMEI):
         current_dict_messages["NB_Journey"] = 0
         current_dict_messages["NB_Msg"] = 0
         current_dict_messages["NB_Heartbeat"] = 0
-        current_dict_messages["Msg_list"] = list()
         current_dict_messages["VIN_list"] = list()
         current_dict_messages["FW_list"] = list()
         current_dict_messages["Mapping_OBD_list"] = list()
         current_dict_messages["Mapping_Diag_list"] = list()
         current_dict_messages["Account_Number"] = ''
+        current_dict_messages["Msg_list"] = list()
 
-def ecrire_dictionnaire_messages(path, strIMEI):
+def ecrire_dictionnaire_messages(chemin, strIMEI):
     global current_dict_messages
-    nom_fic = path + strIMEI + '.json'
+    nom_fic = chemin + strIMEI + '.json'
     if "NB_Msg" in current_dict_messages:
         if current_dict_messages["NB_Msg"] > 0:
             #il y a des informations à sauvegarder
