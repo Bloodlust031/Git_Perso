8e01d6c789dbae36532119f316edeb1cf27adf73
Bloodlust031 <39981246+Bloodlust031@users.noreply.github.com>
2020-11-23 05:39:17 +0100

    Update Extract_VCI_files_json.py

--- Python_Eclipse/ProjetPython/TestJson/Extract_VCI_files_json.py
+++ Python_Eclipse/ProjetPython/TestJson/Extract_VCI_files_json.py
@@ -7,18 +7,19 @@ Created on 6 nov. 2020
 import os
 import json
 import time
+import fnmatch
 
 
-
-to_integrate_Msg_bin = False
+to_integrate_Msg_bin = True
 to_integrate_Msg_raw = False
 to_integrate_Msg_cnt = False
 
 
 
-chemin_base = 'H:/Boulot/Boulot Jeje/Main/Ican/Extract_traces_FTP/TempDownAWSS3/'
-#chemin_base = 'D:/Boulot/Main/Ican/Extract_traces_FTP/TempDownAWSS3/'
-#chemin_base = 'D:/temp/DWLD msg Bastides/'
+chemin_base = 'H:/Boulot/Boulot Jeje/Main/Ican/Extract_traces_FTP/TempDownAWSS3'
+#chemin_base = 'C:\TempDownAWSS3'
+#chemin_base = 'D:\Boulot\Main\Ican\Extract_traces_FTP\TempDownAWSS3'
+#chemin_base = 'D:\temp\DWLD msg Bastides'
 path_sortie = 'D:/temp/'
 prefixe_nom_sortie = 'Msg_IMEI_'
 
@@ -43,17 +44,18 @@ def traite_1_1fic(nom_fic_msg):
     
     with open(nom_fic_msg) as json_file2:
         data = json.load(json_file2)
-        #print(data['tim'], data['int'])
-        #print(data['evt'], data['eid'])
-        #print(os.path.basename(path), data['ime'], data['tim'], data['int'], ecart_reception, data['evt'], data['eid'])
-        #chaine = str(os.path.basename(path))+ " " + str(data['ime'])+ " " + str(data['tim'])+ " " + str(data['int'])+ " " + str(ecart_reception)+ " " + str(data['evt'])+ " " + str(data['eid'])+ '\n'
         fic_IMEI = str(data['ime'])
-        #if (fic_IMEI != current_IMEI):
         if (fic_IMEI != current_IMEI):
             #Changement d'IMEI, on sauvegarde les données courrantes avant d'ouvrir les données du nouvel IMEI
-            ecrire_dictionnaire_messages(path_sortie+prefixe_nom_sortie, current_IMEI)
+            ecrire_dictionnaire_messages(current_IMEI)
             current_IMEI = fic_IMEI
-            lire_dictionnaire_messages(path_sortie+prefixe_nom_sortie, current_IMEI)
+            lire_dictionnaire_messages(current_IMEI)
+            
+        #traitement du nom du fichier
+        current_msg['NomFichier_Msg'] = os.path.basename(nom_fic_msg)
+        str_temp = str(os.path.dirname(nom_fic_msg))
+        str_temp = str_temp.replace("\\","/")
+        current_msg['AdresseFichier_Msg'] = str_temp
             
         #incrémentation des compteurs
         current_dict_messages["NB_Msg"] += 1
@@ -71,8 +73,23 @@ def traite_1_1fic(nom_fic_msg):
         current_msg['Delai_GSM'] = (data['int']-data['tim'])/1000
         current_msg['VIN'] = str(data['vin'])
         
+        #décomposition du message brut
+        decomposition_fichier_brut(str(data['bin']))
+        
+        
         if current_msg['VIN'] not in current_dict_messages["VIN_list"]:
             current_dict_messages["VIN_list"].append(current_msg['VIN'])
+        
+        if "20033" in data['cnt']:
+            str_temp =  data['cnt']['20033']
+            if str_temp not in current_dict_messages["FW_list"]:
+                current_dict_messages["FW_list"].append(str_temp)
+
+        if "160" in data['cnt']:
+            str_temp =  data['cnt']['160']
+            if (str_temp != "0"): 
+                current_dict_messages["Account_Number"] = str_temp
+
         
         if to_integrate_Msg_bin:
             current_msg['Msg_brut'] = str(data['bin'])
@@ -86,9 +103,28 @@ def traite_1_1fic(nom_fic_msg):
         current_dict_messages["Msg_list"].append(dict(current_msg))
     current_msg.clear()
 
-def lire_dictionnaire_messages(chemin, strIMEI):
+def convert_Hex_decimal(str_raw):
+    print("convert_Hex_decimal" + str_raw)
+    nombre = 1  #TODO
+    return nombre
+    
+def decomposition_fichier_brut(raw_data):
+    Liste_param = dict()
+    fini = False
+    str_raw = raw_data[22:]
+    taille = convert_Hex_decimal(str_raw[0:8])
+    str_raw = raw_data[8:]
+     while not fini:
+         fini = True
+    return Liste_param
+
+def get_nom_fic_sortie(strIMEI):
+    nom = path_sortie + prefixe_nom_sortie + strIMEI + '.json'
+    return nom
+
+def lire_dictionnaire_messages(strIMEI):
     global current_dict_messages
-    nom_fic = chemin + strIMEI + '.json'
+    nom_fic = get_nom_fic_sortie(strIMEI)
     current_dict_messages.clear()
     if os.path.isfile(nom_fic):
         with open(nom_fic, 'r') as json_file_result:
@@ -106,9 +142,9 @@ def lire_dictionnaire_messages(chemin, strIMEI):
         current_dict_messages["Account_Number"] = ''
         current_dict_messages["Msg_list"] = list()
 
-def ecrire_dictionnaire_messages(chemin, strIMEI):
+def ecrire_dictionnaire_messages(strIMEI):
     global current_dict_messages
-    nom_fic = chemin + strIMEI + '.json'
+    nom_fic = get_nom_fic_sortie(strIMEI)
     if "NB_Msg" in current_dict_messages:
         if current_dict_messages["NB_Msg"] > 0:
             #il y a des informations à sauvegarder
@@ -117,13 +153,13 @@ def ecrire_dictionnaire_messages(chemin, strIMEI):
             pass
             current_dict_messages.clear()
 
-def efface_dictionnaire_messages(chemin, prefixe):
-    debut_nom_fichier = chemin+prefixe
-    for root, dirs, files in os.walk(chemin): 
-        for i in files: 
-            nom_fic = os.path.join(root, i)
-            if debut_nom_fichier in nom_fic:
-                os.remove(nom_fic)
+
+def efface_dictionnaire_messages():
+    liste_fic = os.listdir(path_sortie)
+    
+    for file1 in liste_fic:
+        if fnmatch.fnmatch(file1, prefixe_nom_sortie + '*.json'):
+            os.remove(path_sortie+file1)
 
 
 if __name__ == "__main__":
@@ -134,14 +170,15 @@ if __name__ == "__main__":
     liste_fichiers = listdirectory(chemin_base)
     nb_fic = 0
     
-    efface_dictionnaire_messages(path_sortie,prefixe_nom_sortie)
+    efface_dictionnaire_messages()
     
     for nom_fic in liste_fichiers:
+        #print(nom_fic)
         traite_1_1fic(nom_fic)
         nb_fic = nb_fic+1
     
    
-    ecrire_dictionnaire_messages(path_sortie+prefixe_nom_sortie, current_IMEI)
+    ecrire_dictionnaire_messages(current_IMEI)
 
     time2 = time.process_time()
     print( time2-time1, "secondes d'executions")
