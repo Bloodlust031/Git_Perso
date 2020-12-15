7435adee8b00cb4184417b3d1fb5c6dc4968b035
Bloodlust031 <39981246+Bloodlust031@users.noreply.github.com>
2020-12-15 18:36:19 +0100

    gfdshfs

--- Python_Eclipse/ProjetPython/TestJson/Import_D2HUB_Info.py
+++ Python_Eclipse/ProjetPython/TestJson/Import_D2HUB_Info.py
@@ -8,6 +8,7 @@ Created on 5 déc. 2020
 import os
 import Configuration
 import json
+import csv
 
 item_list = list()
 
@@ -34,19 +35,84 @@ def get_item_dico(Ligne_txt):
     if (len(ligne_split[4]) >= 1):
         current_item["VEH_Immat"] = ligne_split[4]
     if (len(ligne_split[5]) >= 1):
-        current_item["VEH_Model"] = ligne_split[5]
+        current_item["VEH_MarkModel"] = ligne_split[5]
     if (len(ligne_split[6]) >= 1):
-        current_item["Account_Number"] = ligne_split[6]
+        current_item["Account_ID"] = ligne_split[6]
+    if (len(ligne_split[7]) >= 1):
+        current_item["VEH_Label"] = ligne_split[7]
+    if (len(ligne_split[8]) >= 1):
+        current_item["VEH_VIN"] = ligne_split[8]
+    current_item["Item_Serial"] = ligne_split[9]
     return current_item
+    
     
 def Import_from_ExportD2HUB():
     liste_lignes = list()
 
+def get_item_dico_ExportD2HUBcsv(ligne):
+    current_item = dict()
+    current_item.clear()
+    current_item["Item_IMEI"] = ligne["IMEI"]
+    current_item["Item_Type"] = ligne["Device type"]
+    current_item["Item_FW"] = ligne["Current firmware version"]
+    current_item["Item_Serial"] = ligne["Serial number"]
+    if (len(ligne["Customer account"]) >= 1):
+        current_item["Account_Name"] = ligne["Customer account"]
+    if (len(ligne["Registration"]) >= 1):
+        current_item["VEH_Immat"] = ligne["Registration"]
+    if (len(ligne["Brand"]) >= 1):
+        current_item["VEH_Mark"] = ligne["Brand"]
+    if (len(ligne["Model"]) >= 1):
+        current_item["VEH_Model"] = ligne["Model"]
+    if (len(ligne["Serie"]) >= 1):
+        current_item["VEH_Serie"] = ligne["Serie"]
+    if (len(ligne["Vehicle name"]) >= 1):
+        current_item["VEH_Label"] = ligne["Vehicle name"]
+    if (len(ligne["VIN"]) >= 1):
+        current_item["VEH_VIN"] = ligne["VIN"]  
+    if (len(ligne["last OTA update Request Date"]) >= 1):
+        current_item["Last_OTA_QUERY_Date"] = ligne["last OTA update Request Date"]  
+    if (len(ligne["Date of the first reco message received"]) >= 1):
+        current_item["First_RECO_Date"] = ligne["Date of the first reco message received"]  
+    if (len(ligne["Date of the last message received"]) >= 1):
+        current_item["Last_Msg_Date"] = ligne["Date of the last message received"]  
+    if (len(ligne["Current service"]) >= 1):
+        current_item["Service_Name"] = ligne["Current service"]  
+    return current_item
+
+def Import_from_ExportD2HUBcsv():
+    global item_list
+    item_list.clear()
+    
+    with open(Configuration.path_ExportD2HUBcsv, mode='r', newline='', encoding='utf-8-sig') as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=';')
+        liste_champs = csv_reader.fieldnames
+        if "Type de boitier" in liste_champs:
+            print ("Export en français. Il faut utiliser l'export généré en anglais")
+        else:
+            if "Device type" in liste_champs:
+                #print ("export en anglais")
+                for ligne in csv_reader:
+                    #print (ligne)
+                    item_list.append(get_item_dico_ExportD2HUBcsv(ligne))
+    pass
+    
+        
+    #print (csv_reader)
+    with open('D:\Temp_JSON\OUTPUT\exportD2HubCSV.json', 'w') as json_file_result:
+        json.dump(item_list, json_file_result)
+    pass 
+    
+
             
 if __name__ == '__main__':
     Import_from_ImportD2HUB()
-    with open('D:\Temp_JSON\INPUT_D2HUB/genericInfo.json', 'w') as json_file_result:
+    with open('D:\Temp_JSON\OUTPUT\genericInfo.json', 'w') as json_file_result:
         json.dump(item_list, json_file_result)
-    pass 
+    pass
+
+    Import_from_ExportD2HUBcsv()
+   
+   
    
     pass
--- Python_Eclipse/ProjetPython/TestJson/Telech_AWS_Json.py
+++ Python_Eclipse/ProjetPython/TestJson/Telech_AWS_Json.py
@@ -5,17 +5,45 @@ Created on 30 nov. 2020
 @author: blood
 '''
 import os
+import json
 import Configuration
 
 #commande =  "aws s3 sync s3://ican.processed.d2hub.fr/2020-11-27/MARKETIP/864504031504844/ H:/Boulot/TempDownAWSS3/MARKETIP/864504031504844/"
 #os.system("start " + commande)
 
 #strreq = "cmd /c ""echo Téléchargement en cours..... Merci de ne pas fermer cette fenêtre && aws s3 sync s3://ican.processed.d2hub.fr/2020-11-27/MARKETIP/864504031504844/ H:/Boulot/TempDownAWSS3/MARKETIP/864504031504844/ >nul 2>&1"""
+#start aws s3 sync s3://ican.processed.d2hub.fr/2020-11-22/MARKETIP/867322034083212 / D:\Temp_JSON\INPUT_Msg/MARKETIP/867322034083212 /
+#start aws s3 sync s3://ican.processed.d2hub.fr/2020-11-22/SDPV/867322034083212 / D:\Temp_JSON\INPUT_Msg/MARKETIP/867322034083212 /
+#start aws s3 sync s3://ican.processed.d2hub.fr/2020-11-22/UNMATCHED/867322034083212 / D:\Temp_JSON\INPUT_Msg/UNMATCHED/867322034083212 /
+#start aws s3 sync s3://ican.invalid.d2hub.fr/2020-11-22//867322034083212 / D:\Temp_JSON\INPUT_Msg/INVALID/867322034083212 /
 
 liste_cmd_bucket = list()
 liste_cmd_bucket2 = list()
 liste_cmd_UNMATCHED = list()
 liste_cmd_invalid = list()
+
+def maj_configuration():
+    Configuration.set_Date_list('2020-11-22', '2020-12-14')
+    Configuration.set_IMEI_List(['867322034083212','867322034092015','867322034083212','867322034105809'])
+
+def Supprim_Event_msg():
+    print("Effacement des messages Event")
+    current_dict_messages = dict()
+    current_dict_messages.clear()
+    
+    for root, dirs, files in os.walk(Configuration.Chemin_json): 
+        for fichier in files: 
+            nom_fichier = os.path.join(root, fichier)
+            with open(nom_fichier, 'r') as json_file_result:
+                current_dict_messages = json.load(json_file_result)
+            pass
+            if 'evt' in current_dict_messages:
+                if (str(current_dict_messages['evt']) == '100'):
+                    #message event à supprimer
+                    if (str(current_dict_messages['eid']) != '2A'):
+                        os.remove(nom_fichier)
+            current_dict_messages.clear()
+
 
 def gen_liste_cmd():
     global liste_cmd_bucket
@@ -26,17 +54,22 @@ def gen_liste_cmd():
     liste_cmd_bucket2.clear()
     liste_cmd_UNMATCHED.clear()
     liste_cmd_invalid.clear()
+    
     for jour in Configuration.Date_list:
         for imei in Configuration.IMEI_list:
             str_temp = "start aws s3 sync s3://ican.processed.d2hub.fr/" + jour + Configuration.Bucket + imei +  "/ " + Configuration.Chemin_json + Configuration.Bucket + imei + "/"
             liste_cmd_bucket.append(str_temp)
+            #print(str_temp)
             if (len(Configuration.Bucket2) > 1):
                 str_temp = "start aws s3 sync s3://ican.processed.d2hub.fr/" + jour + Configuration.Bucket2 + imei +  "/ " + Configuration.Chemin_json + Configuration.Bucket + imei + "/"
                 liste_cmd_bucket2.append(str_temp)
+                #print(str_temp)
             str_temp = "start aws s3 sync s3://ican.processed.d2hub.fr/" + jour + "/UNMATCHED/" + imei +  "/ " + Configuration.Chemin_json + "/UNMATCHED/" + imei + "/"
             liste_cmd_UNMATCHED.append(str_temp)
+            #print(str_temp)
             str_temp = "start aws s3 sync s3://ican.invalid.d2hub.fr/" + jour + "//" + imei +  "/ " + Configuration.Chemin_json + "/INVALID/" + imei + "/"
             liste_cmd_invalid.append(str_temp)
+            #print(str_temp)
 
 def execute_cmd():
     nb_req = len(liste_cmd_bucket) + len(liste_cmd_bucket2) + len(liste_cmd_UNMATCHED) + len(liste_cmd_invalid)
@@ -60,7 +93,18 @@ def execute_cmd():
     
 
 if __name__ == '__main__':
+    maj_configuration()
     gen_liste_cmd()
     execute_cmd()
+    
+    bretour = False
+    txt_input = input("Voulez vous supprimer les messages Event (O pour oui) ?")
+    try:
+        if ((txt_input[0] == "O") or (txt_input[0] == "o")):
+            bretour = True
+    except:
+        bretour = False
+    if bretour:
+        Supprim_Event_msg()
     
     pass
