e925c9bdf8de61fdef09659f8eb18bcb5a16ddd4
Jerome DEVAY <jerome.devay@actia.fr>
2021-04-11 08:16:54 +0200

    Create Diction_Ordon.py

---
+++ Python_Eclipse/ProjetPython/ExercicesOpenClassroom/Diction_Ordon.py
@@ -0,0 +1,17 @@
+'''
+Created on 8 avr. 2021
+
+@author: jdevay
+'''
+
+class Diction_Ordon:
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
